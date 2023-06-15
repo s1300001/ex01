ce120cb8fbdebc4f20bad135ed7ebe7c12ec8ad5
AIZAWA Keita <s1300001@sshsv211.u-aizu.ac.jp>
2023-06-15 23:48:00 +0900

    Greetings added

--- sugoroku.java
+++ sugoroku.java
@@ -2,6 +2,11 @@ import java.util.Random;
 import java.util.Scanner;
 public class sugoroku{
     public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner sc= new Scanner(System.in);
+        String name =sc.nextLine();
+        System.out.println("Hello,"+name+"!");
+        
         Random x=new Random();
         System.out.println("Rolling dice...");
         int n1=x.nextInt(6)+1;
